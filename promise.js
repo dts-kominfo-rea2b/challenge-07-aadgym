3563d274d1bb4e08f86d248ee8c844544ce44987
aadgym <aad_gym@outlook.com>
2022-06-20 19:19:05 +0700

    promise fullfilled

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const promiseOutput = async (emosi) => {
+  const arrayXXI = await promiseTheaterIXX();
+  const arrayVGC = await promiseTheaterVGC();
+  const arrayFilm = arrayXXI.concat(arrayVGC);
+  const arrayEmosi = arrayFilm.filter(x => x.hasil == emosi)
+  const sumEmosi = arrayEmosi.length
+  return sumEmosi;
+}
+//const promiseOutput = asyn;
 
 module.exports = {
   promiseOutput,
